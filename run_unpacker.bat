d30b8908b828216c24d6fb8dae8360a3ebac42bc
M4XL1M1T <centsgames@gmail.com>
2025-08-31 17:36:12 +0200

    Update run_unpacker.bat

--- run_unpacker.bat
+++ run_unpacker.bat
@@ -1,8 +1,7 @@
 @echo off
 REM Aktiviere das virtuelle Environment
 call .venv\Scripts\activate
-REM Installiere die benötigten Pakete
-pip install -r requirements.txt
 REM Starte das Python-Skript
 python unpackerlister.py
 pause
+
